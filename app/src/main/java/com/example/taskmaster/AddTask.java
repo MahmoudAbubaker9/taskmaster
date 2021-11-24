818926b2a027b77b056c85a459e20f3ba267fe79
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-11-24 03:18:59 +0200

    lab32

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -5,10 +5,14 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
 
 public class AddTask extends AppCompatActivity {
 
@@ -26,10 +30,18 @@ public class AddTask extends AppCompatActivity {
                 String titles = addTitle.getText().toString();
                 String bodys = addBody.getText().toString();
                 String states = addState.getText().toString();
-                Task addNewtask = new Task(titles,bodys,State.IN_PROGRESS);
-                TaskDatabase db = Room.databaseBuilder(getApplicationContext(),TaskDatabase.class, "database-task").allowMainThreadQueries().build();
-                TaskDao taskDao = db.userDao();
-                taskDao.savedata(addNewtask);
+//                Task addNewtask = new Task(titles,bodys,State.IN_PROGRESS);
+//                TaskDatabase db = Room.databaseBuilder(getApplicationContext(),TaskDatabase.class, "database-task").allowMainThreadQueries().build();
+//                TaskDao taskDao = db.userDao();
+//                taskDao.savedata(addNewtask);
+
+                Task task = Task.builder().title(titles).body(bodys).state(states).build();
+
+                Amplify.API.mutate(
+                        ModelMutation.create(task),
+                        response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
+                        error -> Log.e("MyAmplifyApp", "Create failed", error)
+                );
                 Intent goToMain = new Intent(AddTask.this, MainActivity.class);
                 startActivity(goToMain);
             }
