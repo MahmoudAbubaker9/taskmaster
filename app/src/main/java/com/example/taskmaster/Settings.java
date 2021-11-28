8d4c437aec071b68286a8185e902d31165d5c34e
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-11-28 03:22:44 +0200

    lab33 not finish

--- app/src/main/java/com/example/taskmaster/Settings.java
+++ app/src/main/java/com/example/taskmaster/Settings.java
@@ -8,6 +8,7 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
@@ -25,9 +26,25 @@ public class Settings extends AppCompatActivity {
         public void onClick(View v) {
             EditText UserNameEdit = findViewById(R.id.UserNameEdit);
             String UserName = UserNameEdit.getText().toString();
+            RadioButton radioButton1=findViewById(R.id.radioSitting1);
+            RadioButton radioButton2=findViewById(R.id.radioSitting2);
+            RadioButton radioButton3=findViewById(R.id.radioSitting3);
+
+            String teamSelection = null;
+            if(radioButton1.isChecked()){
+                teamSelection="1";
+            }
+            else if(radioButton2.isChecked()){
+                teamSelection="2";
+            }
+            else if(radioButton3.isChecked()){
+                teamSelection="3";
+            }
+
 
             SharedPreferences ShareUserName = PreferenceManager.getDefaultSharedPreferences(Settings.this);
             ShareUserName.edit().putString("UserName",UserName +"  Tasks").apply();
+            ShareUserName.edit().putString("Team",teamSelection ).apply();
             Toast.makeText(getApplicationContext(), "submitted!",Toast.LENGTH_LONG).show();
         }
     });
